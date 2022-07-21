31081c33bf6f7cd888f5314d87ccba18c126deb0
splincode <omaxphp@yandex.ru>
2022-07-21 10:43:48 +0000

    Update snapshots/demo/next/v3.x to output generated at a441d20

--- 50436.12f0b46155b5ef812993.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[50436],{50436:t=>{t.exports="export default \"@import 'taiga-ui-local';\\n\\n.ticks-labels {\\n    .tui-slider-ticks-labels(m);\\n}\\n\";"}}]);
---
+++ 50436.8cec5cfdc27da0d690c7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_taiga_ui_components=self.webpackChunk_taiga_ui_components||[]).push([[50436],{50436:n=>{n.exports="export default \"@import 'taiga-ui-local';\\n\\n.ticks-labels {\\n    .tui-slider-ticks-labels(m);\\n}\\n\\n.tick-label {\\n    .clearbtn();\\n    outline: 0;\\n    cursor: pointer;\\n}\\n\";"}}]);
