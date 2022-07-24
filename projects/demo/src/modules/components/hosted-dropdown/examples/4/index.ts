17fe38c186e41fa020e7ff3c56bc9e7b35e87220
splincode <splincodewd@yandex.ru>
2022-07-24 19:36:20 +0300

    chore(demo): add example in `hosted-dropdown`

--- projects/demo/src/modules/components/hosted-dropdown/examples/4/index.ts
+++ projects/demo/src/modules/components/hosted-dropdown/examples/4/index.ts
@@ -1,14 +1,23 @@
 import {Component} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 import {changeDetection} from '@demo/emulate/change-detection';
 import {encapsulation} from '@demo/emulate/encapsulation';
+import {BehaviorSubject} from 'rxjs';
 
 @Component({
     selector: 'tui-hosted-dropdown-example-4',
     templateUrl: './index.html',
+    styleUrls: ['./index.less'],
     changeDetection,
     encapsulation,
 })
 export class TuiHostedDropdownExample4 {
+    readonly settingsDropdownOpened$ = new BehaviorSubject(false);
+
+    readonly testForm = new FormGroup({
+        option: new FormControl(false),
+    });
+
     open = false;
 
     index = 0;
@@ -16,5 +25,9 @@ export class TuiHostedDropdownExample4 {
     onClick(): void {
         this.open = false;
         this.index = 1;
+    }
+
+    handleFiltersDropdownOpenedChange(open: boolean): void {
+        this.settingsDropdownOpened$.next(open);
     }
 }
